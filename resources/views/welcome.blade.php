6c620a824000c5e02f6a22bc8aa48eb0a712a866
jcdl96 <ipreferjc@gmail.com>
2019-01-16 15:55:55 +0800

    test commit

--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <h1>Care Group Reporting</h1>
-    </div>
-</div>
-@endsection
