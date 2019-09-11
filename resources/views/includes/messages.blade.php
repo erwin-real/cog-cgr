5f53c8c248cee7a6d41dd294c97ed6e6ec801a6b
erwin-real <erwinsantoscapati@gmail.com>
2019-09-12 01:04:49 +0800

    Updated Alert Messages

--- resources/views/includes/messages.blade.php
+++ resources/views/includes/messages.blade.php
@@ -1,19 +1,22 @@
 @if(count($errors) > 0)
     @foreach($errors->all() as $error)
-        <div class="mt-3 mb-0 alert alert-danger">
-            {{$error}}
+        <div class="mt-3 mb-0 alert alert-danger alert-dismissible">
+            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+            <strong>Error!</strong> {{$error}}
         </div>
     @endforeach
 @endif
 
 @if(session('success'))
-    <div class="mt-3 mb-0 alert alert-success">
-        {{session('success')}}
+    <div class="mt-3 mb-0 alert alert-success alert-dismissible">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        <strong>Success!</strong> {{session('success')}}
     </div>
 @endif
 
 @if(session('error'))
-    <div class="mt-3 mb-0 alert alert-danger">
-        {{session('error')}}
+    <div class="mt-3 mb-0 alert alert-danger alert-dismissible">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        <strong>Error!</strong> {{session('error')}}
     </div>
 @endif
